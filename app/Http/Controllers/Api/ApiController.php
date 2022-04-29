3b9fd50f80db1253191fc7eaef061970532e376b
faiqalizade <faiq.alizade.89@mail.ru>
2022-04-29 12:41:32 +0400

    Product resource controller (Create,Update,Show,Get,Destroy)

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Repository\Product\ProductRepositoryInterface;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+/**
+ *
+ */
+class ApiController extends Controller
+{
+
+    public function getResponse($action): \Illuminate\Http\JsonResponse
+    {
+        if (!$action['error']) {
+            $res = [
+                'error' => false,
+                'msg' => 'Successfully',
+            ];
+            if(isset($action['data']) && $action['data']){
+                $res['data'] = $action['data'];
+            }
+            return response()->json($res);
+        }
+
+        return response()->json([
+            'error' => true,
+            'msg' => $action['msg'],
+        ], $action['code']);
+    }
+
+}
---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Repository\Product\ProductRepositoryInterface;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+/**
+ *
+ */
+class ProductController extends ApiController
+{
+
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private ProductRepositoryInterface $repository;
+
+    /**
+     * @param ProductRepositoryInterface $productRepository
+     */
+    public function __construct(ProductRepositoryInterface $productRepository)
+    {
+        $this->repository = $productRepository;
+    }
+
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request): \Illuminate\Http\JsonResponse
+    {
+
+        $rules = [
+            'product_name' => 'min:3|string',
+            'category_name' => 'min:3|string',
+            'price_from' => 'int',
+            'price_to' => 'int',
+            'not_deleted' => 'int|in:0,1',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => true,
+                'errors' => $validator->errors(),
+                'msg' => 'Validator error',
+                'data' => [],
+            ], 422);
+        }
+
+        $data = $this->repository->getProductsByRequest($request, $request->get('not_deleted'));
+
+        return response()->json([
+            'error' => false,
+            'msg' => 'Successfully',
+            'data' => $data,
+        ]);
+    }
+
+
+    public function show($id)
+    {
+        $product = $this->repository->getProduct($id);
+        if (!$product) {
+            return response()->json([
+                'error' => false,
+                'msg' => 'Product not found',
+            ], 404);
+        }
+
+        $product->load('categories');
+
+
+        return response()->json([
+            'error' => false,
+            'msg' => 'Successfully',
+            'data' => $product,
+        ]);
+    }
+
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $rules = [
+            'title' => 'required|min:3',
+            'price' => 'required|int',
+            'categories' => 'required|array|min:2|max:10|exists:categories,id',
+            'categories.*' => 'required|int|distinct',
+            'status' => 'required|int|in:0,1'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => true,
+                'errors' => $validator->errors(),
+                'msg' => 'Validator error'
+            ], 422);
+        }
+
+        $action = $this->repository->create($request);
+
+        return $this->getResponse($action);
+
+    }
+
+
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id): \Illuminate\Http\JsonResponse
+    {
+        $rules = [
+            'title' => 'required|min:3',
+            'price' => 'required|int',
+            'categories' => 'required|array|min:2|max:10|exists:categories,id',
+            'categories.*' => 'required|int|distinct'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => true,
+                'errors' => $validator->errors(),
+                'msg' => 'Validator error'
+            ], 422);
+        }
+
+        $action = $this->repository->update($id, $request);
+
+        return $this->getResponse($action);
+    }
+
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id): \Illuminate\Http\JsonResponse
+    {
+        $action = $this->repository->delete($id);
+
+        return $this->getResponse($action);
+    }
+}
