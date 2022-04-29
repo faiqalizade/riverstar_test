482e6ca6c415270a291f761506923c0464ca4074
faiqalizade <faiq.alizade.89@mail.ru>
2022-04-29 12:55:41 +0400

    Migration changes

--- database/migrations/2022_04_28_153924_create_product_category_rel_table.php
+++ database/migrations/2022_04_28_153924_create_product_category_rel_table.php
@@ -15,11 +15,10 @@ return new class extends Migration {
         Schema::create('category_product', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('product_id')
-                ->unsigned()
-                ->nullable();
+                ->unsigned();
+
             $table->bigInteger('category_id')
-                ->unsigned()
-                ->nullable();
+                ->unsigned();
 
             $table->softDeletes();
 
