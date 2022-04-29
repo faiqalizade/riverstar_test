dc0586078896e0c10fe0dc88adce6e2ccaaea0a6
faiqalizade <faiq.alizade.89@mail.ru>
2022-04-29 15:32:23 +0400

    Add resource new fields

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -12,6 +12,7 @@ use JetBrains\PhpStorm\ArrayShape;
  * @property mixed $categories
  * @property mixed $price
  * @property mixed $created_at
+ * @property mixed $status
  */
 class ProductResource extends JsonResource
 {
@@ -19,12 +20,13 @@ class ProductResource extends JsonResource
      * @param Request $request
      * @return array
      */
-    #[ArrayShape(['id' => "mixed", 'title' => "mixed", 'price' => "mixed", 'categories' => "\App\Http\Resources\CategoryCollection", 'created_at' => "mixed"])] public function toArray($request): array
+    #[ArrayShape(['id' => "mixed", 'title' => "mixed", 'price' => "mixed", 'status' => "mixed", 'created_at' => "mixed", 'categories' => "\App\Http\Resources\CategoryCollection"])] public function toArray($request): array
     {
         return [
             'id' => $this->id,
             'title' => $this->title,
             'price' => $this->price,
+            'status' => $this->status,
             'created_at' => $this->created_at,
             'categories' => new CategoryCollection($this->categories),
         ];
